e600c15a417e709b5af4413016ce01614ad52dd7
kavyasukumar <kavya.sukumar@outlook.com>
2014-12-06 19:36:54 -0500

    initial commit. Has only cover page.

---
+++ script/customizations.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $('#cover').height($(window).height() + 'px');
+    var width = $('#imgs').width();
+    $('#imgs img').width(width + 'px');
+
+    $('#imgs').on('mouseover',function(){
+        $('.highlight').stop().animate({ width: '100%' },500);
+    }).on('mouseout', function () {
+        $('.highlight').stop().animate({ width: '0%' },500);
+    })
+
+    $('.collapsed').click(function () {
+        $(this).removeClass('collapsed');
+        $(window).scrollTop($(this).position().top- $('.navbar').height());
+    })
+});
